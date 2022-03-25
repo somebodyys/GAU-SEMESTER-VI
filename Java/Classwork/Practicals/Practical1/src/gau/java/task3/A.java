75fe7defe4d515266b11a91d4cb2a74aa7d8e978
Tornike Kikacheishvili <tkokikaca1@gmail.com>
2022-03-25 16:11:47 +0400

    Task 3

--- Java/Classwork/Practicals/Practical1/src/gau/java/task3/A.java
+++ Java/Classwork/Practicals/Practical1/src/gau/java/task3/A.java
@@ -1,5 +1,27 @@
 package gau.java.task3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class A {
 
+    public static void method1(){
+        int a, b, evenCounter = 0, oddCounter = 0;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            int randNumber = random.nextInt(a, b);
+            System.out.println(randNumber);
+            if(randNumber % 2 == 1){
+                oddCounter++;
+            } else {
+                evenCounter++;
+            }
+        }
+
+        System.out.printf("Odds : %d\nEven : %d", oddCounter, evenCounter);
+    }
 }
