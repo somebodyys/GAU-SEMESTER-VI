9d0b738c76e77c1515ff65432247b97f7d9774d8
Tornike Kikacheishvili <tkokikaca1@gmail.com>
2022-06-24 14:44:40 +0400

    Export SQL

---
+++ Java/Exams/project/java_project_questions.sql
@@ -0,0 +1,6 @@
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (1, 'In what year were the first Air Jordan sneakers re', '1984', 10);
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (2, 'Which African country was formerly known as Abyssinia?', 'Ethiopia', 9);
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (3, 'In a bingo game, which number is represented by the phrase “two little ducks”?', '22', 12);
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (6, 'In which European city would you find Orly airport?', 'Paris', 4);
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (7, 'Which singer’s real name is Stefani Joanne Angelina Germanotta?', 'Lady Gada', 2);
+INSERT INTO java_project.questions (id, question, answer, score) VALUES (8, 'Fissures, vents, and plugs are all associated with which geological feature?', 'Volcanos', 5);
