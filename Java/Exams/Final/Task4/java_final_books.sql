77acd4fe2921676c7c8d9edeb7c5a6e739113dcc
Tornike Kikacheishvili <tkokikaca1@gmail.com>
2022-06-24 14:49:10 +0400

    Re commmit

---
+++ Java/Exams/Final/Task4/java_final_books.sql
@@ -0,0 +1,9 @@
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (1, 'test', 'test', '2022-06-08', 22);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (2, 'test', 'test', '2022-05-30', 53);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (3, 'Tornike', 'asdawd', '2022-06-07', 344);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (4, 'Test', 'Test', '2022-06-07', 22);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (5, 'My Items', 'toesada', '2022-06-13', 33);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (6, 'new ', 'Imtes', '2022-06-07', 22);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (7, 'te', 'te', '2022-05-30', 33);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (8, 'a', 'a', '2022-05-30', 2);
+INSERT INTO java_final.books (id, name, author, release_date, price) VALUES (9, 'a', 'a', '2022-05-30', 2);
